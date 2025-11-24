1969ac045d7c540211dfa2bbd02cbe84a2c77655
David Hobley <david.hobley@sharpblue.com.au>
2025-11-24 17:26:46 +1000

    minor refactoring

--- lib/widgets/progress_bar.dart
+++ lib/widgets/progress_bar.dart
@@ -16,22 +16,21 @@ class ProgressBar extends ConsumerWidget {
     var progressAsync = ref.watch(progressProvider(book.uri));
 
     String pageNumbers = '';
-
     if (progressAsync.hasValue) {
       ReadingProgress progress = progressAsync.value!;
 
-      // Don't display a page number on the title page
-      pageNumbers = book.parsingBook || progress.chapterNumber == 0
-          ? ''
-          : '${book.currentPageNumber(progress.chapterNumber, progress.pageNumber)}-${book.nextChapterPageNumber(progress.chapterNumber)}/${book.totalPages}';
+      if (book.parsingBook) {
+        pageNumbers = 'Parsing eBook; please be patient';
+      } else {
+        pageNumbers = progress.chapterNumber > 0 ? '${book.currentPageNumber(progress.chapterNumber, progress.pageNumber)}-${book.nextChapterPageNumber(progress.chapterNumber)}/${book.totalPages}' : '';
+      }
     }
     return Padding(
       padding: EdgeInsets.fromLTRB(6, 3, 6, 3),
       child: Stack(
         children: [
           Align(alignment: Alignment.centerLeft, child: Text(book.title, style: Theme.of(context).textTheme.labelSmall)),
-          if (book.parsingBook) ...[Align(alignment: Alignment.center, child: Text('Parsing eBook; please be patient', textAlign: TextAlign.center, style: Theme.of(context).textTheme.labelSmall))],
-          if (!book.parsingBook) ...[Align(alignment: Alignment.center, child: Text(pageNumbers, textAlign: TextAlign.center, style: Theme.of(context).textTheme.labelSmall))],
+          Align(alignment: Alignment.center, child: Text(pageNumbers, textAlign: TextAlign.center, style: Theme.of(context).textTheme.labelSmall)),
           Align(alignment: Alignment.centerRight, child: Text(book.author, textAlign: TextAlign.right, style: Theme.of(context).textTheme.labelSmall)),
         ],
       ),
