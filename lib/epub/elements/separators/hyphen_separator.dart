17648a94d03cd80965d562777403c5043e426f87
David Hobley <david.hobley@sharpblue.com.au>
2025-12-13 13:42:00 +1100

    make hyphens a little more obvious and a little better positioned

--- lib/epub/elements/separators/hyphen_separator.dart
+++ lib/epub/elements/separators/hyphen_separator.dart
@@ -8,8 +8,15 @@ class HyphenSeparator extends Separator {
   }
 
   @override
+  void getConstraints() {
+    // I want to ensure Hyphens are a little more visible when displaying them.
+    super.getConstraints();
+    width = width * 1.5;
+  }
+
+  @override
   void paint(Canvas c, double height, double xPos, double yPos) {
-    c.drawLine(Offset(xPos+1, yPos+(height/2)+1), Offset(xPos+width-1, yPos+(height/2)+1), Paint()..color = Colors.black);
+    c.drawLine(Offset(xPos+1, yPos+(height/2)+2), Offset(xPos+width-1, yPos+(height/2)+2), Paint()..color = Colors.black);
   }
 
   @override
