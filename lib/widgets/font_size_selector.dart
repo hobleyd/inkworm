3916dfd9e62fb4468572e04c175cc4b474c7366d
David Hobley <david.hobley@sharpblue.com.au>
2025-12-19 16:17:26 +1100

    emsure font sizes are visible in settings

--- lib/widgets/font_size_selector.dart
+++ lib/widgets/font_size_selector.dart
@@ -39,33 +39,9 @@ class _FontSize extends ConsumerState<FontSize> {
           padding: EdgeInsetsGeometry.only(left: size.leftIndent, bottom: 12),
           child: Text('Select your preferred font size:', style: Theme.of(context).textTheme.labelMedium),
         ),
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: List.generate(9, (index) {
-              int fontSize = 12 + index;
-              return Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 4),
-                child: ElevatedButton(
-                  onPressed: () {
-                    ref.read(themeProvider.notifier).setFontSize(fontSize.toDouble());
-                    setState(() {
-                      this.fontSize = fontSize;
-                      _setFontSize();
-                    });
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: progress?.fontSize == fontSize ? Colors.blue : Colors.grey[300],
-                    foregroundColor: progress?.fontSize == fontSize ? Colors.white : Colors.black,
-                  ),
-                  child: Text('${fontSize}pt', style: TextStyle(fontSize: fontSize.toDouble()),),
-                ),
-              );
-            },
-            ),
-          ),
-        ),
+        Row(mainAxisAlignment: MainAxisAlignment.center, children: _getSizedFontRow(12, 6),),
+        SizedBox(height: 12),
+        Row(mainAxisAlignment: MainAxisAlignment.center,children: _getSizedFontRow(18, 6),),
         Padding(
           padding: EdgeInsetsGeometry.only(left: size.leftIndent, top: 12, bottom: 12),
           child: CheckboxListTile(
@@ -83,6 +59,29 @@ class _FontSize extends ConsumerState<FontSize> {
         ),
       ],
     );
+  }
+
+  List<Widget> _getSizedFontRow(int start, int length) {
+    return List.generate(length, (index) {
+      int fontSize = start + index;
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 4),
+        child: ElevatedButton(
+          onPressed: () {
+            ref.read(themeProvider.notifier).setFontSize(fontSize.toDouble());
+            setState(() {
+              this.fontSize = fontSize;
+              _setFontSize();
+            });
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: progress?.fontSize == fontSize ? Colors.blue : Colors.grey[300],
+            foregroundColor: progress?.fontSize == fontSize ? Colors.white : Colors.black,
+          ),
+          child: Text('${fontSize}pt', style: TextStyle(fontSize: fontSize.toDouble()),),
+        ),
+      );
+    });
   }
 
   void _setFontSize() {
