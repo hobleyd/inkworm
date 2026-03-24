5ac19a64507c8b0602de9c699a8d71c1c94c4e6e
David Hobley <david.hobley@sharpblue.com.au>
2026-03-24 21:05:00 +1100

    add chapter parsing into the passed object

--- lib/epub/parser/isolates/requests/open_epub_request.dart
+++ lib/epub/parser/isolates/requests/open_epub_request.dart
@@ -1,26 +1,14 @@
 import 'dart:isolate';
 
 import 'package:get_it/get_it.dart';
-import 'package:inkworm/epub/interfaces/isolate_parse_request.dart';
-import 'package:inkworm/epub/parser/isolates/responses/book_details_response.dart';
 import 'package:xml/xml.dart';
 
 import '../../../../models/page_size.dart';
-import '../../../cache/link_cache.dart';
-import '../../../cache/text_cache.dart';
-import '../../../handlers/block_handler.dart';
-import '../../../handlers/css_handler.dart';
-import '../../../handlers/image_handler.dart';
-import '../../../handlers/inline_handler.dart';
-import '../../../handlers/line_break_handler.dart';
-import '../../../handlers/link_handler.dart';
-import '../../../handlers/superscript_handler.dart';
-import '../../../handlers/text_handler.dart';
-import '../../../structure/build_line.dart';
-import '../../../structure/build_page.dart';
+import '../../../interfaces/isolate_parse_request.dart';
 import '../../css_parser.dart';
 import '../../epub_parser.dart';
 import '../../extensions.dart';
+import '../responses/book_details_response.dart';
 
 class OpenEpubRequest extends IsolateParseRequest {
   final String   css;
@@ -36,18 +24,6 @@ class OpenEpubRequest extends IsolateParseRequest {
     GetIt.instance.registerSingleton<PageSize>(PageSize());
     GetIt.instance.registerSingleton<CssParser>(CssParser());
     GetIt.instance.registerSingleton<EpubParser>(EpubParser());
-    GetIt.instance.registerSingleton<BlockHandler>(BlockHandler());
-    GetIt.instance.registerSingleton<TextHandler>(TextHandler());
-    GetIt.instance.registerSingleton<LineBreakHandler>(LineBreakHandler());
-    GetIt.instance.registerSingleton<InlineHandler>(InlineHandler());
-    GetIt.instance.registerSingleton<LinkHandler>(LinkHandler());
-    GetIt.instance.registerSingleton<ImageHandler>(ImageHandler());
-    GetIt.instance.registerSingleton<SuperscriptHandler>(SuperscriptHandler());
-    GetIt.instance.registerSingleton<CssHandler>(CssHandler());
-    GetIt.instance.registerSingleton<BuildLine>(BuildLine());
-    GetIt.instance.registerSingleton<BuildPage>(BuildPage());
-    GetIt.instance.registerSingleton<LinkCache>(LinkCache());
-    GetIt.instance.registerSingleton<TextCache>(TextCache());
   }
 
   @override
--- lib/epub/parser/isolates/requests/parse_chapter_request.dart
+++ lib/epub/parser/isolates/requests/parse_chapter_request.dart
@@ -1,21 +1,64 @@
 import 'dart:isolate';
 
+import 'package:get_it/get_it.dart';
 import 'package:inkworm/epub/interfaces/isolate_parse_request.dart';
+import 'package:inkworm/epub/parser/isolates/responses/chapter_response.dart';
 
+import '../../../../models/page_size.dart';
+import '../../../cache/link_cache.dart';
+import '../../../cache/text_cache.dart';
+import '../../../handlers/block_handler.dart';
+import '../../../handlers/css_handler.dart';
+import '../../../handlers/image_handler.dart';
+import '../../../handlers/inline_handler.dart';
+import '../../../handlers/line_break_handler.dart';
+import '../../../handlers/link_handler.dart';
+import '../../../handlers/superscript_handler.dart';
+import '../../../handlers/text_handler.dart';
 import '../../../interfaces/isolate_parse_response.dart';
+import '../../../structure/build_line.dart';
+import '../../../structure/build_page.dart';
+import '../../../structure/epub_chapter.dart';
+import '../../css_parser.dart';
+import '../../epub_parser.dart';
 
 class ParseChapterRequest extends IsolateParseRequest {
   ParseChapterRequest({required super.id, required super.href});
 
+  bool initComplete = false;
+
   @override
   void init() {
-
+    GetIt.instance.registerSingleton<PageSize>(PageSize());
+    GetIt.instance.registerSingleton<CssParser>(CssParser());
+    GetIt.instance.registerSingleton<EpubParser>(EpubParser());
+    GetIt.instance.registerSingleton<BlockHandler>(BlockHandler());
+    GetIt.instance.registerSingleton<TextHandler>(TextHandler());
+    GetIt.instance.registerSingleton<LineBreakHandler>(LineBreakHandler());
+    GetIt.instance.registerSingleton<InlineHandler>(InlineHandler());
+    GetIt.instance.registerSingleton<LinkHandler>(LinkHandler());
+    GetIt.instance.registerSingleton<ImageHandler>(ImageHandler());
+    GetIt.instance.registerSingleton<SuperscriptHandler>(SuperscriptHandler());
+    GetIt.instance.registerSingleton<CssHandler>(CssHandler());
+    GetIt.instance.registerSingleton<BuildLine>(BuildLine());
+    GetIt.instance.registerSingleton<BuildPage>(BuildPage());
+    GetIt.instance.registerSingleton<LinkCache>(LinkCache());
+    GetIt.instance.registerSingleton<TextCache>(TextCache());
   }
 
   @override
-  Future<IsolateParseResponse> process(SendPort uiPort) {
-    // TODO: implement process
-    throw UnimplementedError();
+  Future<void> process(SendPort uiPort) async {
+    if (!initComplete) {
+      init();
+      initComplete = true;
+    }
+    EpubParser parser = GetIt.instance.get<EpubParser>();
+    try {
+      EpubChapter chapter = await parser.parseChapter(id, href);
+      uiPort.send(ChapterResponse(chapter: chapter));
+    } catch (e, s) {
+      uiPort.send(IsolateParseResponse(error: e.toString(), stacktrace: s.toString()));
+    }
   }
 
 }
