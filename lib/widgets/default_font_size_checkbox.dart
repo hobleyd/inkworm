24508976eb4f1be7a52d55a36a006ce5746c9aea
David Hobley <david.hobley@sharpblue.com.au>
2026-04-02 19:42:31 +1000

    minor refactor of the font size settings to deal with async providers correctly

---
+++ lib/widgets/default_font_size_checkbox.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get_it/get_it.dart';
+
+
+import '../models/page_size.dart';
+import '../providers/font_size.dart';
+
+class DefaultFontSizeCheckbox extends ConsumerWidget {
+  final int selectedFontSize;
+
+  const DefaultFontSizeCheckbox({super.key, required this.selectedFontSize});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    PageSize size = GetIt.instance.get<PageSize>();
+    var defaultFontSizeAsync = ref.watch(fontSizeProvider);
+
+    return defaultFontSizeAsync.when(error: (error, stackTrace) {
+      return const Text("It's time to panic; we find a default font size!");
+    }, loading: () {
+      return const Center(child: CircularProgressIndicator());
+    }, data: (int fontSize) {
+      return Padding(
+            padding: EdgeInsetsGeometry.only(left: size.leftIndent, top: 12, bottom: 12),
+            child: CheckboxListTile(
+              value: selectedFontSize == fontSize,
+              onChanged: (bool? setDefault) {
+                  if (setDefault != null && setDefault) {
+                    ref.read(fontSizeProvider.notifier).setDefaultFontSize(selectedFontSize);
+                  }
+              },
+              title: Text('Make this font the new default (for new books)', style: Theme.of(context).textTheme.labelMedium),
+            ),
+          );
+    });
+  }
+}
--- lib/widgets/font_size_selector.dart
+++ lib/widgets/font_size_selector.dart
@@ -6,9 +6,9 @@ import 'package:get_it/get_it.dart';
 import '../models/page_size.dart';
 import '../models/reading_progress.dart';
 import '../providers/epub.dart';
-import '../providers/font_size.dart';
 import '../providers/progress.dart';
 import '../providers/theme.dart' hide Theme;
+import 'default_font_size_checkbox.dart';
 
 class FontSize extends ConsumerStatefulWidget {
   const FontSize({super.key,});
@@ -18,7 +18,6 @@ class FontSize extends ConsumerStatefulWidget {
 }
 
 class _FontSize extends ConsumerState<FontSize> {
-  ReadingProgress? progress;
   int fontSize = 12;
   bool setDefaultFont = false;
 
@@ -27,41 +26,30 @@ class _FontSize extends ConsumerState<FontSize> {
     PageSize size = GetIt.instance.get<PageSize>();
     var progressAsync = ref.watch(progressProvider);
 
-    if (progressAsync.hasValue) {
-      progress = progressAsync.value;
-      fontSize = progress!.fontSize;
-    }
+    return progressAsync.when(error: (error, stackTrace) {
+      return const Text("It's time to panic; we can't make any progress!");
+    }, loading: () {
+      return const Center(child: CircularProgressIndicator());
+    }, data: (var progress) {
+      fontSize = progress.fontSize;
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Padding(
-          padding: EdgeInsetsGeometry.only(left: size.leftIndent, bottom: 12),
-          child: Text('Select your preferred font size:', style: Theme.of(context).textTheme.labelMedium),
-        ),
-        Row(mainAxisAlignment: MainAxisAlignment.center, children: _getSizedFontRow(12, 6),),
-        SizedBox(height: 12),
-        Row(mainAxisAlignment: MainAxisAlignment.center,children: _getSizedFontRow(18, 6),),
-        Padding(
-          padding: EdgeInsetsGeometry.only(left: size.leftIndent, top: 12, bottom: 12),
-          child: CheckboxListTile(
-            value: setDefaultFont,
-            onChanged: (bool? setDefault) {
-              setState(() {
-                setDefaultFont = setDefault!;
-                if (setDefaultFont) {
-                  _setFontSize();
-                }
-              });
-            },
-            title: Text('Make this font the new default (for new books)', style: Theme.of(context).textTheme.labelMedium),
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: EdgeInsetsGeometry.only(left: size.leftIndent, bottom: 12),
+            child: Text('Select your preferred font size:', style: Theme.of(context).textTheme.labelMedium),
           ),
-        ),
-      ],
-    );
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: _getSizedFontRow(12, 6, progress),),
+          SizedBox(height: 12),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: _getSizedFontRow(18, 6, progress),),
+          DefaultFontSizeCheckbox(selectedFontSize: progress.fontSize,)
+        ],
+      );
+    });
   }
 
-  List<Widget> _getSizedFontRow(int start, int length) {
+  List<Widget> _getSizedFontRow(int start, int length, ReadingProgress progress) {
     return List.generate(length, (index) {
       int fontSize = start + index;
       return Padding(
@@ -71,12 +59,12 @@ class _FontSize extends ConsumerState<FontSize> {
             ref.read(themeProvider.notifier).setFontSize(fontSize.toDouble());
             setState(() {
               this.fontSize = fontSize;
-              _setFontSize();
             });
+            _setFontSize(progress.copyWith(book: progress.book, fontSize: fontSize, chapterNumber: progress.chapterNumber, pageNumber: 0));
           },
           style: ElevatedButton.styleFrom(
-            backgroundColor: progress?.fontSize == fontSize ? Colors.blue : Colors.grey[300],
-            foregroundColor: progress?.fontSize == fontSize ? Colors.white : Colors.black,
+            backgroundColor: progress.fontSize == fontSize ? Colors.blue : Colors.grey[300],
+            foregroundColor: progress.fontSize == fontSize ? Colors.white : Colors.black,
           ),
           child: Text('${fontSize}pt', style: TextStyle(fontSize: fontSize.toDouble()),),
         ),
@@ -84,14 +72,9 @@ class _FontSize extends ConsumerState<FontSize> {
     });
   }
 
-  void _setFontSize() {
-    if (progress != null) {
-      ref.read(progressProvider.notifier).setProgress(progress!.book, fontSize, progress!.chapterNumber, progress!.pageNumber);
-      ref.read(epubProvider.notifier).openBook(progress!.book);
-
-      if (setDefaultFont) {
-        ref.read(fontSizeProvider.notifier).setDefaultFontSize(fontSize);
-      }
-    }
+  void _setFontSize(ReadingProgress progress) async {
+    // Reset page to 0 as they'll change with a different font size.
+    await ref.read(progressProvider.notifier).setProgress(progress.book, progress.fontSize, progress.chapterNumber, progress.pageNumber);
+    ref.read(epubProvider.notifier).resetBook(progress.book);
   }
 }
