036f634c36face383728240b7bbac3a02df50de2
David Hobley <david.hobley@sharpblue.com.au>
2026-04-25 13:46:24 +1000

    fix image width calculations

--- lib/epub/handlers/image_handler.dart
+++ lib/epub/handlers/image_handler.dart
@@ -6,6 +6,7 @@ import 'package:inkworm/epub/parser/css_parser.dart';
 import 'package:xml/xml.dart';
 
 import '../../models/element_size.dart';
+import '../../models/page_size.dart';
 import '../content/html_content.dart';
 import '../content/image_content.dart';
 import '../parser/epub_parser.dart';
@@ -45,21 +46,22 @@ class ImageHandler extends HtmlHandler {
       double requiredHeight = result.height;
 
       CssParser cssParser = GetIt.instance.get<CssParser>();
+      PageSize  pageSize  = GetIt.instance.get<PageSize>();
       String?  widthString = cssParser.getStringAttribute(element, elementStyle, 'width');
       String? heightString = cssParser.getStringAttribute(element, elementStyle, 'height');
 
-      if (widthString != null) {
-        if (widthString.endsWith('%')) {
-          double percent = double.parse(widthString.substring(0, widthString.length-1)) / 100;
-          requiredWidth = requiredWidth * percent;
-          requiredHeight = requiredHeight * percent;
-        } else if (heightString?.endsWith('%') ?? false) {
-          double percent = double.parse(heightString!.substring(0, heightString.length-1)) / 100;
-          requiredWidth = requiredWidth * percent;
-          requiredHeight = requiredHeight * percent;
-        } else {
-          requiredWidth = double.parse(widthString  ?? '0');
-          requiredHeight = double.parse(heightString ?? '0');
+      if (widthString != null && widthString.endsWith('%')) {
+        double percent = double.parse(widthString.substring(0, widthString.length - 1)) / 100;
+        requiredWidth  = pageSize.canvasWidth * percent;
+        requiredHeight = result.height * (requiredWidth / result.width);
+      } else if (heightString != null && heightString.endsWith('%')) {
+        double percent = double.parse(heightString.substring(0, heightString.length - 1)) / 100;
+        requiredHeight = pageSize.canvasHeight * percent;
+        requiredWidth  = result.width * (requiredHeight / result.height);
+      } else if (widthString != null) {
+        requiredWidth = double.tryParse(widthString) ?? 0;
+        if (heightString != null) {
+          requiredHeight = double.tryParse(heightString) ?? 0;
         }
       }
 
